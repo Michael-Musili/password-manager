1898d0f6932ab1a616bf0c6083c6d02a15d307d2
Michael-Musili <michaelmuttuku@gmail.com>
2022-02-20 15:50:04 +0300

    added a class to generate new users

---
+++ password-locker/password.py
@@ -0,0 +1,14 @@
+class User:
+     '''
+     class that generate new instances of user
+     '''
+     user_list =[],
+
+     def _init_(self,username,password):
+
+         '''
+         user properties
+         '''
+         
+         self.username = username
+         self.password =password
