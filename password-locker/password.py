0ea3fbe47d36414ab08cfccdda6d68e7f4a17061
Michael-Musili <michaelmuttuku@gmail.com>
2022-02-20 19:15:28 +0300

    added new method

--- password-locker/password.py
+++ password-locker/password.py
@@ -1,10 +1,14 @@
-class User:
-     '''
-     class that generate new instances of user
-     '''
-     user_list =[],
+import random
+import string
+import pyperclip
 
-     def _init_(self,username,password):
+class User:
+    '''
+    class that generate new instances of user
+    '''
+    user_list =[],
+
+    def _init_(self,username,password):
 
          '''
          user properties
@@ -12,3 +16,27 @@ class User:
          
          self.username = username
          self.password =password
+
+    def save_user(self):
+        """
+        this  method  saves a new userto the user list
+        """
+        User.user_list.append(self)
+
+
+    def delete_user(self):
+        '''
+        this method deletes a user account
+        '''
+        User.user_list.remove(self)
+
+
+    @classmethod
+    def find_user(cls, username):
+        '''
+        this method finds username 
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return  user
+
